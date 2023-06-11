ad587fa27dde66882527ab6fc3f583e263997c2f
Sourav Sharma <69393883+souravsharm@users.noreply.github.com>
2023-06-11 20:15:17 +1000

    Update tello_drone.py

--- Creadit/tello_drone.py
+++ Creadit/tello_drone.py
@@ -17,43 +17,11 @@ class DroneFlying(Node):
 
     def send_command(self, msg):
         command = msg.data
-        x= 50
         if command == 'Takeoff':
             self.drone.takeoff()
             time.sleep(2)
         elif command == 'Right':
-            self.drone.move_right(x)
-        elif command == 'Forward':
-            self.drone.move_forward(x)
-        elif command == 'Backward':
-            self.drone.move_back(x)
-        elif command == 'Battery':
-            battery = self.drone.get_battery()
-            self.get_logger().info(str(battery))
-
-        elif command == 'Mission':
-
-            x =100
-            y =0
-            z=50
-            speed= 30
-            
-            self.drone.enable_mission_pads()
-            time.sleep(1)
-            mid = self.drone.get_mission_pad_id()
-            self.get_logger().info(str(mid))
-            time.sleep(1)
-            if(mid == 1):
-                self.drone.go_xyz_speed_mid(x, y, z, speed, mid)
-        elif command == 'Flip':
-            self.drone.flip_back()
-        elif command == 'Capture':
-            frame = self.drone.get_frame_read().frame
-            image_path = os.path.join('Image', f'image_{self.image_counter}.jpg')
-            cv2.imwrite(image_path, frame)
-            self.get_logger().info(f'Saved image: {image_path}')
-            self.image_counter += 1
-            time.sleep(0.5)
+            self.drone.move_right(50)
         elif command == 'Land':
             self.drone.land()
 
